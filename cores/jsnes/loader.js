595b02269eee54344ccfc48e375c1fd60f16f0d9
micahkamei23-star <micahkamei23@gmail.com>
2026-03-16 01:19:37 -0700

    Create loader file for JSNES build

---
+++ cores/jsnes/loader.js
@@ -0,0 +1,8 @@
+/* JSNES - JavaScript NES Emulator
+* Version: 0.0.1
+* License: MIT
+* https://unpkg.com/jsnes/dist/jsnes.min.js
+*/
+
+// The actual JSNES code will be inserted here in the build,
+// preserving this loader.
